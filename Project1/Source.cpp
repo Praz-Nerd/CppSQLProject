bce81f7c0d8383222d86c6bda80158a8563cb5dd
Praz-Nerd <stefan.vladescu2012@gmail.com>
2023-11-05 15:30:11 +0200

    Updated .gitignore Added a test project

---
+++ Project1/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Test code, hello Fyps :)\n";
+	cout << "O sa fac alt proiect in the same repo si acela va fi cel pe care lucram\n";
+	return 0;
+}
